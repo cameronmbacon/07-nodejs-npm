0570137ef945570b042c8aa51324dde8d6dc8780
Michael Padget <mpadget@comcast.net>
2017-03-22 17:32:29 -0700

    Completed to-do items, stretch goals, and 404 page

--- starter-code/server.js
+++ starter-code/server.js
@@ -13,12 +13,22 @@ const PORT = process.env.PORT || 3000;
 // TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+// TODO: Using the response object, send the index.html file back to the user
+app.get('/', function(request, response) {
+  response.sendFile('public/index.html', {root: '.'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
-app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+app.get('/new', function(request, response) {
+  response.sendFile('public/new.html', {root: '.'});
 });
+
+  // TODO: Log to the console a message that lets you know which port your server has started on
+app.listen(PORT, function() {
+  console.log('your app is being served on localhost:3000');
+})
+
+app.use(function(request, response, next) {
+  console.log('oh noes a 404')
+  response.status(404).sendFile('404.html', { root:'./public'})
+})
